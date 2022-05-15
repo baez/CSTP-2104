7f22245b60dc82310d8752748fb9eb34ee01abb4
LorenzCastillo <lorenz5524@hotmail.com>
2022-05-15 00:20:27 -0700

    Assignment 2 changes

--- Assignment 2/Tasks/RunTasks.cs
+++ Assignment 2/Tasks/RunTasks.cs
@@ -14,45 +14,38 @@ namespace Assignment_2.Tasks
             int[] sumArr = new int[100];
             int[] lastNumArr = new int[100];
 
-            // Task A adds all the numbers up
-            Task taskA = Task.Run(() =>
-            {
-                int sum = 0;
-                int lastNum = 0;
+            int sum = 0;
+            int lastNum = 0;
 
-                for (int i = 1; i <= 100; i++)
-                {
+            for (int i = 1; i <= 100; i++)
+            {
+                // Task A adds all the numbers up
+                Task taskA = Task.Run(() =>
+                {                  
                     sum += i;
                     lastNum = sum;
 
                     sumArr[i - 1] = sum;
                     lastNumArr[i - 1] = lastNum;
-                }
 
-            });
-            // Waits for taskA to finish before moving on
-            taskA.Wait();
+                });
+                // Waits for taskA to finish before moving on
+                taskA.Wait();
 
-            // Task B prints the sum of each iteration
-            Task taskB = Task.Run(() =>
-            {
-                Console.WriteLine("Hi");
-                for (int i = 1; i <= 100; i++)
+                // Task B prints the sum of each iteration
+                Task taskB = Task.Run(() =>
                 {
                     Console.WriteLine("{0} + {1} = {2}", i, lastNumArr[i - 1], sumArr[i - 1]);
-                }
-            });
-            // Waits for taskB to finish before moving on
-            taskB.Wait();
+                });
+                // Waits for taskB to finish before moving on
+                taskB.Wait();
 
 
-            // Task C writes the sum to a file at each iteration
-            Task taskC = Task.Run(() =>
-            {
-                string filePath = "../../../Tasks/data.txt";
-
-                for (int i = 1; i <= 100; i++)
+                // Task C writes the sum to a file at each iteration
+                Task taskC = Task.Run(() =>
                 {
+                    string filePath = "../../../Tasks/data.txt";
+
                     string stringInput = "Sum: " + i.ToString() + " + " + lastNumArr[i - 1].ToString() + " = " + sumArr[i - 1].ToString() + Environment.NewLine;
 
                     if (!File.Exists(filePath))
@@ -63,11 +56,12 @@ namespace Assignment_2.Tasks
                     {
                         File.AppendAllText(filePath, stringInput);
                     }
-                }
-            });
-            taskC.Wait();
+                });
+                taskC.Wait();
 
-            Console.WriteLine("\nSaved data to file!");
+                Console.WriteLine("Saved sum #{0} to the file!\n", i);
+            }
+            
         }
     }
 }
