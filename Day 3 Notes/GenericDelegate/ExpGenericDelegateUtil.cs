3f81ce4c9ccdcc85f8ce239f2164e6fd9eedbd7c
LorenzCastillo <lorenz5524@hotmail.com>
2022-05-18 18:48:03 -0700

    Day 3 Notes

---
+++ Day 3 Notes/GenericDelegate/ExpGenericDelegateUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day_3_Notes.GenericDelegate
+{
+    // Generics
+    public delegate List<T> GenDelegate<T>(T[] arg);
+    public delegate List<int> GenDelegate(int arg);
+
+    public class ExpGenericDelegateUtil
+    {
+        public static void TestGenericDelegate()
+        {
+            string[] parts = { "123", "456", "7890" };
+            var getDelegate = new GenDelegate<int>(GenDelFunc);
+            int[] values = { 4, 5, 6, 7 };
+
+            TakeAction(values, getDelegate);
+        }
+
+        public static void TakeAction<T>(T[] values, GenDelegate<T> genDelegate)
+        {
+            var result = genDelegate(values);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(result.ElementAt(i));
+            }
+        }
+
+        public static List<T> GenDelFunc<T>(T[] arg)
+        {
+            return arg.ToList();
+        }
+    }
+}
---
+++ Day 3 Notes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Day_3_Notes.GenericDelegate;
+
+namespace Day_3_Notes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpGenericDelegateUtil.TestGenericDelegate();
+        }
+    }
+}
